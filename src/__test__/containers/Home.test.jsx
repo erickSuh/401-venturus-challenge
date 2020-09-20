264ee79312da2e1890da3d2eafbb19b0a22b3872
Érick Kenji Sugahara <erick.sugahara@gmail.com>
2020-09-20 08:40:56 -0300

    feat: add pre-commit lint and test

--- src/__test__/containers/Home.test.jsx
+++ src/__test__/containers/Home.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../App';
+import App from 'App';
 
 test('render home page', () => {
   const { getByText } = render(<App />);
