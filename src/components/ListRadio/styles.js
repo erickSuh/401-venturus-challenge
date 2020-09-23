0fa17125af5aab1a1998bac4bfc73c955c0777b6
Érick Kenji Sugahara <erick.sugahara@gmail.com>
2020-09-23 17:25:36 -0300

    feat: fizer radio button color

--- src/components/ListRadio/styles.js
+++ src/components/ListRadio/styles.js
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
   padding: 16px;
 
   h3 {
-    color: ${(props) => (props.invalid ? props.theme.colors.invalid : "#aaa")};
+    color: ${(props) => (props.invalid ? props.theme.colors.invalid : '#aaa')};
     font-size: 14px;
   }
 `;
@@ -16,16 +16,34 @@ export const List = styled.div`
   margin-top: 10px;
 
   label {
-    display: flex;
-    align-items: center;
-    color: ${(props) => (props.invalid ? props.theme.colors.invalid : "#aaa")};
+    margin-right: 16px;
+    color: ${(props) => (props.invalid ? props.theme.colors.invalid : '#aaa')};
+  }
 
-    &:not(:last-child) {
-      margin-right: 40px;
-    }
+  input {
+    margin-right: 8px;
+  }
 
-    input {
-      margin-right: 8px;
-    }
+  input[type='radio'] {
+    -webkit-appearance: none;
+    width: 12px;
+    height: 12px;
+    border: 1px solid ${(props) => (props.invalid ? props.theme.colors.invalid : '#aaa')}; 
+    border-radius: 50%;
+    outline: none;
+  }
+  input[type='radio']:hover {
+    box-shadow: 0 0 5px 0px orange inset;
+  }
+  input[type='radio']:before {
+    content:'';
+    display: block;
+    width: 60%;
+    height: 60%;
+    margin: 20% auto;    
+    border-radius: 50%;
+  }
+  input[type='radio']:checked:before {
+    background: ${(props) => (props.invalid ? props.theme.colors.invalid : props.theme.colors.primary)};
   }
 `;
